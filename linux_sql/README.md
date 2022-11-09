2916581415167126aee4d8295c36a4b6f761310c
Kelvin Lam <lam29121996@gmail.com>
2022-11-09 20:58:22 +0000

    finished

--- linux_sql/README.md
+++ linux_sql/README.md
@@ -1,2 +1,58 @@
 # Linux Cluster Monitoring Agent
 This project is under development. Since this project follows the GitFlow, the final work will be merged to the main branch after Team Code Team.
+
+## Introduction
+The Jarvis Linux Cluster Administration (LCA) team manages a Linux cluster of 10 nodes/servers running CentOS 7. These servers are internally connected through a switch and able to communicate through internal IPv4 addresses.
+
+The LCA team needs to record the hardware specifications of each node and monitor node resource usage (e.g. CPU, memory) in real-time and stored in an RDBMS.
+
+The LCA team will use the data to generate reports for future resource planning purposes (e.g. add or remove servers).
+
+## Quick Start
+### Command to create / start / stop PostgreSQL container
+
+`$ ./scripts/psql_docker.sh start|stop|create [db_username][db_password]`
+
+```
+// To create and start PostgreSQL container
+$ ./scripts/psql_docker.sh create <db_username> <db_password>
+
+// To start PostgreSQL container
+$ ./scripts/psql_docker.sh start
+
+// To stop PostgreSQL container
+$ ./scripts/psql_docker.sh stop
+```
+
+### Before any connect to the psql instance
+```
+$ export PGPASSWORD=<db_password>
+```
+
+### Command to connect to the psql instance
+```
+// connect to the psql instance
+$ psql -h localhost -U <db_username> -W
+```
+
+### Command to Data Definition Language (DDL)
+```
+$ psql -h localhost -U postgres -d host_agent -f sql/ddl.sql
+```
+
+
+## Implemenation
+
+### Architecture
+![alt text](./assets/architecture.png)
+
+### Scripts
+
+
+### Database Modeling
+
+## Test
+
+## Deployment
+
+## Improvements
--- linux_sql/scripts/psql_docker.sh
+++ linux_sql/scripts/psql_docker.sh
@@ -14,53 +14,62 @@ container_status=$?
 
 # Switch for different case of $action
 case $action in
-  # Create handler
-  create)
-    # Check if the container already exists; exit 1 if exists
-    if [ $container_status -eq 0 ]; then
-      echo 'Container already exists'
-      exit 1
-    fi
-
-    # Check number of arguments; exit 1 if not equals to 3
-    if [ $# -ne 3 ]; then
-      echo 'Create requires username and password'
-      exit 1
-    fi
-
-    # Create volume; volume name: pgdata
-    docker volume create pgdata
-
-    # Create container; container name: jrvs-psql
-    #                   environment variable POSTGRES_PASSWORD: $db_password
-    #                   detached mode,
-    #                   volume name: pgdata
-    #                   mounted path: /var/lib/postgresql/data
-    #                   published port: hostPort(5432) to containerPort(5432)
-    #                   image: postgres: 9.6-alpine
-    docker run --name jrvs-psql -e POSTGRES_PASSWORD="$db_password" -d -v pgdata:/var/lib/postgresql/data -p 5432:5432 postgres:9.6-alpine
-    exit $?
-    ;;
-
-  # Start | stop handler
-  start | stop)
-    # Check if the container is not created; exit 1 if container is not created
-    if [ $container_status -eq 1 ]; then
-      echo 'Container is not created'
-      exit 1
-    fi
-
-    # Start / stop the container
-    docker container "$action" jrvs-psql
-    exit $?
-    ;;
-
-  # else handler
-  *)
-    echo 'Illegal command'
-    echo 'Commands: start|stop|create'
+# Create handler
+create)
+  # Check if the container already exists; exit 1 if exists
+  if [ $container_status -eq 0 ]; then
+    echo 'Container already exists'
     exit 1
-    ;;
+  fi
+
+  # Check number of arguments; exit 1 if not equals to 3
+  if [ $# -ne 3 ]; then
+    echo 'Create requires username and password'
+    exit 1
+  fi
+
+  # Create volume; volume name: pgdata
+  docker volume create pgdata
+
+  # Create container; mode: detached mode
+  #                   container name: jrvs-psql
+  #                   environment variable:
+  #			                POSTGRES_USER: $db_username
+  #			                POSTGRES_PASSWORD: $db_password
+  #                   volume name: pgdata, /var/lib/postgresql/data is mounted
+  #                   mounted path: /var/lib/postgresql/data
+  #                   published port: hostPort(5432) to containerPort(5432)
+  #                   image: postgres: 9.6-alpine
+  docker run -d \
+    --name jrvs-psql \
+    -e POSTGRES_USER=$db_username \
+    -e POSTGRES_PASSWORD=$db_password \
+    -v pgdata:/var/lib/postgresql/data \
+    -p 5432:5432 \
+    postgres:9.6-alpine
+
+  exit $?
+  ;;
+
+# Start | stop handler
+start | stop)
+  # Check if the container is not created; exit 1 if container is not created
+  if [ $container_status -eq 1 ]; then
+    echo 'Container is not created'
+    exit 1
+  fi
+
+  # Start / stop the container
+  docker container "$action" jrvs-psql
+  exit $?
+  ;;
+
+# else handler
+*)
+  echo 'Illegal command'
+  echo 'Commands: start|stop|create'
+  exit 1
+  ;;
 esac
 
 exit 0
